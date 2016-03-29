405d351f65eb97879fc5f97cc805ceac29be78ef
Maurice Muoneke <obibiman@yahoo.com>
2016-03-29 16:49:55 -0500

    Unit test

---
+++ Biodiversity.Web.Tests/AuthorsControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Biodiversity.DataAccess.SqlDataTier;
+using Biodiversity.DataAccess.SqlDataTier.Repository.Interface;
+using Biodiversity.Web.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Biodiversity.Web.Tests
+{
+    [TestClass]
+    public class AuthorsControllerTests
+    {
+        private Mock<IAuthorRepository> _authorRepository;
+
+        public void Initialize()
+        {
+            _authorRepository = new Mock<IAuthorRepository>();
+            AuthorsController authorController = new AuthorsController();
+        }
+
+        [TestMethod]
+        public void Index_Accepts_SearchString_And_Returns_List()
+        {
+            
+        }
+    }
+}
